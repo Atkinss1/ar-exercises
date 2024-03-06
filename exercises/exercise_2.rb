faba6b6ba250fd9b34e0ba3b0dd3398502207e78
Atkinss <jordanatkins@live.com>
2024-03-05 20:03:00 -0700

    find by ID, edit name and save to database

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+
+p @store1
+
+@store2 = Store.find_by(id: 2)
+
+p @store2
+
+@store1.name = "Edmonton"
+
+
+p @store1.name
