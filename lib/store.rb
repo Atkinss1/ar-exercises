d068a3ed53e6d8835bcdad3727341235f9355ad0
Atkinss <jordanatkins@live.com>
2024-03-05 21:17:14 -0700

    add: validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  before_save :apparel_stocked
+
+  def apparel_stocked
+    unless womens_apparel || mens_apparel
+      errors.add(:base, "You must carry one of the apparels")
+    end
+  end
+
 end
