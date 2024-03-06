3cd0c9e052c7ac22e4be96dc8f6c80902d5ee877
Atkinss <jordanatkins@live.com>
2024-03-05 21:16:38 -0700

    add: user_prompt for store name, add store with missing arguements, print errors

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "What should we call the store name? "
+store_name = gets.chomp.to_s
+
+new_store = Store.new(name: store_name)
+
+unless new_store.save
+  puts "Errors occurred while create the store:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
+
